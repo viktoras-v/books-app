4503d13bde58ab46fcb8dbd89edee258dc3aa2f9
viktoras.v <viktoras.v@outlook.com>
2026-02-26 19:51:16 +0200

    ci

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+
+# Create EC2 MySql
 resource "aws_instance" "app" {
-  ami           = var.ami
-  instance_type = "t3.micro"
-  key_name      = var.key_name
-
+  ami           = "ami-01f79b1e4a5c64257"
+  instance_type = "t2.micro"
+  key_name      = "my_key"
   vpc_security_group_ids = [aws_security_group.app.id]
-
+  subnet_id = aws_subnet.private_subnet.id
+  associate_public_ip_address = true
   tags = {
-    Name = "books-app"
+    Name = "ec2-book-app"
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+#variable "az" {
+#  type    = string
+#  default = "eu-central-1a"
+}
+
+#variable "ami_id" {
+#  type = string
+#}
+
+#variable "subnet_ids" {
+#  type = list(string)
+#  default = [
+#  "subnet-aaa",
+#  "subnet-bbb"
+#]
+#}
+
+#variable "cidr_block" {
+#  type = string
+#  default = "10.0.0.0/16"
+#}
+
+#3variable "public_subnet_cidr" {
+#  type = string
+#  default = "10.0.1.0/24"
+#}
+
+#variable "private_subnet_cidr" {
+#  type = string
+#  default = "10.0.10.0/24"
+#}
+
+#variable "my_ip" {
+#  type = string
+#  default = "0.0.0.0/0"
+#}
+
+
+
